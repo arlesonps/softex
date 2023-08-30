ac1cb833f4f56fcde40122341151183749a934f6
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-08-30 09:04:15 -0300

    criando variaves
    Exercicio:
    Estamos rodeados por dados no nosso cotidiano
    e, nas aulas, aprendemos sobre os seus diversos
    tipos. Agora, vamos criar variáveis do tipo
    boolean, BigInt, string e symbol. No primeiro
    momento, após declará-las, precisamos exibir
    seus conteúdos, que devem ser UNDEFINED. Em
    seguida, vamos deixar essas variáveis com
    valores nulos e imprimir. Por fim, cada uma deve
    receber um dos tipos como atribuição e os seus
    respectivos valores devem ser impressos na tela.

--- javascriptorientacaoobjeto/exercicio2.js
+++ javascriptorientacaoobjeto/exercicio2.js
@@ -1,1 +1,39 @@
+// Criando variáveis
+let booleanVariaves;
+let bigIntVariaves;
+let stringVariaves;
+let symbolVariaves;
+
+// Exibindo conteúdos iniciais (undefined)
+console.log("Conteúdo das variáveis antes de atribuição:");
+console.log("booleanVariaves:", booleanVariaves);
+console.log("bigIntVariaves:", bigIntVariaves);
+console.log("stringVariaves:", stringVariaves);
+console.log("symbolVariaves:", symbolVariaves);
+
+// Atribuindo valores nulos às variáveis
+booleanVariaves = null;
+bigIntVariaves = null;
+stringVariaves = null;
+symbolVariaves = null;
+
+// Exibindo valores nulos
+console.log("\nValores nulos das variáveis:");
+console.log("booleanVariaves:", booleanVariaves);
+console.log("bigIntVariaves:", bigIntVariaves);
+console.log("stringVariaves:", stringVariaves);
+console.log("symbolVariaves:", symbolVariaves);
+
+// Atribuindo valores aos tipos de variáveis
+booleanVariaves = true;
+bigIntVariaves = 1234567890123456789012345678901234567890n;
+stringVariaves = "Olá, mundo FAP!";
+symbolVariaves = Symbol("mySymbol");
+
+// Exibindo valores atribuídos
+console.log("\nValores atribuídos às variáveis:");
+console.log("booleanVariaves:", booleanVariaves);
+console.log("bigIntVariaves:", bigIntVariaves);
+console.log("stringVariaves:", stringVariaves);
+console.log("symbolVariaves:", symbolVariaves);
 
