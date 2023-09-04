0832369d236edd8f66a41bcf4d6212e21af653f5
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-09-04 14:57:17 +0000

    atualização

---
+++ JavaScript Orientação Objeto/exercicio3.js
@@ -0,0 +1,11 @@
+// Declaração das variáveis
+let nome = "Arleson";
+let salario = 3800.00;
+let idade = 35;
+let possuiDiploma = true;
+
+// Exibindo os dados cadastrados
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui Diploma: " + possuiDiploma);
